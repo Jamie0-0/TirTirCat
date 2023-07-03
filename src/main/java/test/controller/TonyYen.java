ccac1953db273399901dd04c799dcf12edc0f44b
TonyYen <bmw266b7540@gmail.com>
2023-07-03 16:42:24 +0800

    Merge branch 'main' of https://github.com/Jamie0-0/TirTirCat
    # Conflicts:
    #	src/main/java/test/controller/TonyYen.java

--- src/main/java/test/controller/TonyYen.java
+++ src/main/java/test/controller/TonyYen.java
@@ -3,7 +3,11 @@ package test.controller;
 public class TonyYen {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
+		System.out.println("安安安安3版");
+=======
 		System.out.println("安安安安版本3");
+>>>>>>> ada27617da7964a53703d9afaa66b8c242679c7c
 	}
 
 }
