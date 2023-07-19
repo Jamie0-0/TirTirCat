ffc2739ea4913426e8bc51f01577595663e83096
TonyYen <bmw266b7540@gmail.com>
2023-07-20 01:19:08 +0800

    add hibernate and all settings except your own class mapping

--- src/main/java/articles/vo/Comment.java
+++ src/main/java/articles/vo/Comment.java
@@ -2,13 +2,31 @@ package articles.vo;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
 	private Integer com_id;
+	@Column
 	private Integer com_art_id;
+	@Column 
 	private Integer com_user_id;
+	@Column 
 	private String com_content;
+	@Column 
 	private Timestamp com_date_time;
+	@Column 
 	private Integer com_rep_count;
+	@Column 
 	private String com_status;
 
 	public Integer getCom_id() {
